736a0ee89013045156dff479640d1cb56d87d08a
wolfgirl416 <47046043+wolfgirl416@users.noreply.github.com>
2019-01-25 21:34:34 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# wwtbamDiscordBot
+# zesty-tut
+For educational purposes.
--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const discordBot = require('./discordBot.js');
-const token = process.env.DISCORD_TOKEN || require('./config/config.js').token;
-if (token == "") {
-    throw new Error("Bot Token was not Specified");
-}
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
 
-discordBot.login(token);
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity(`Hey, Wassup!`);
+});
+
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}ping`){
+    message.channel.send("Pong!");
+  }
+});
+
+bot.login(process.env.token);
